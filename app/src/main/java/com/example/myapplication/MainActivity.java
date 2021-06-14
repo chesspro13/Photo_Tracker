dbd7ea04c3b01456457e293f72990236a67f705b
chesspro13 <brandon@mauldin314.com>
2021-06-13 18:01:09 -0600

    init

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,119 @@
+package com.example.myapplication;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.DexterError;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.PermissionRequestErrorListener;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.io.IOException;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button internetRequest;
+    String BASE_URL = "http://192.168.0.112:5000/";
+    OkHttpClient client = new OkHttpClient();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void connectServer(View v){
+        EditText ipv4AddressView = findViewById(R.id.IPAddress);
+        String ipv4Address = ipv4AddressView.getText().toString();
+        EditText portNumberView = findViewById(R.id.portNumber);
+        String portNumber = portNumberView.getText().toString();
+
+        String postUrl= "http://"+ipv4Address+":"+portNumber+"/";
+        postUrl = "http://192.168.0.112:5000/";
+
+        String postBodyText="Hello";
+        MediaType mediaType = MediaType.parse("text/plain; charset=utf-8");
+        RequestBody postBody = RequestBody.create(mediaType, postBodyText);
+
+        postRequest(MainActivity.this, postBody);
+        //getRequest(MainActivity.this, postBody);
+
+    }
+
+
+    public void postRequest(Activity act, RequestBody postBody) {
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("http://192.168.0.112:5000")
+                .post(postBody)
+                .build();
+
+        Toast.makeText(act, "Bill", Toast.LENGTH_SHORT).show();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                // Cancel the post on failure.
+
+                call.cancel();
+
+                // In order to access the TextView inside the UI thread, the code is executed inside runOnUiThread()
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView responseText = findViewById(R.id.responseText);
+                        responseText.setText("Failed to Connect to Server");
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                // In order to access the TextView inside the UI thread, the code is executed inside runOnUiThread()
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView responseText = findViewById(R.id.responseText);
+                        try {
+                            responseText.setText(response.body().string());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
+
+        Toast.makeText(act, "Cypher", Toast.LENGTH_SHORT).show();
+    }
+}
